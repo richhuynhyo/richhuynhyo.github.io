1e9c5ae4adad5d0699d99be91463537912c7defb
richhyaa <richhuynhdev@gmail.com>
2017-02-02 09:22:46 -0800

    site change

--- js/app.js
+++ js/app.js
@@ -7,16 +7,10 @@ var mouseY = 0;
 //Events
 $(document).ready(function () {
 
-	//#f2f020
-	//extrudedText(element_id, depth, rgb, stretch, shadow)
-	extrudedText("title", 40, [242, 240, 32], .35, 1);
-	extrudedText("name", 45, [242, 240, 32], .35, 1);
-
 	//Load Model
 	updateWrapWidthHeight();
 	initModel();
 	animateModel();
-	headerFade();
 
 	//ADD EVENT LISTENERS
 	initRayPolyHover()
@@ -40,24 +34,3 @@ function onWindowResize()
 	WindowResizeModel();
 }
 
-
-
-
-function headerFade()
-{
-	var pos = $(window).scrollTop();
-	if (pos < WRAP_HEIGHT)
-	{
-		var grayPercent = Math.ceil((pos / WRAP_HEIGHT) * 100);
-		var gray = "grayscale(" + grayPercent.toString() + "%)";
-		var opacity = 1 - ((pos*.8) / (WRAP_HEIGHT));
-		$("header").css("filter", gray);
-		$("header").css("opacity", opacity);
-	}
-	else if (pos >= WRAP_HEIGHT)
-	{
-		$("header").css("filter", "grayscale(100%)");
-		$("header").css("opacity", ".2");
-	}
-
-}
--- js/renderModel.js
+++ js/renderModel.js
@@ -3,7 +3,6 @@ var camera, scene;
 var canvasRenderer, webglRenderer;
 var mesh, zmesh, geometry;
 
-//var particle;
 
 function initModel()
 {
@@ -20,18 +19,6 @@ function initModel()
 	directionalLight.position.set(0, 40, 100).normalize();
 	scene.add(directionalLight);
 
-	//PARTICLES
-	//var spriteMat = new THREE.SpriteMaterial({
-	//	map: new THREE.CanvasTexture(generateSprite()),
-	//	blending: THREE.AdditiveBlending
-	//});
-	//for (var i = 0; i < 750; i++)
-	//{
-	//	particle = new THREE.Sprite(spriteMat);
-	//	initParticle(particle, i * 17.5);
-	//	scene.add(particle);
-	//}
-
 	//RENDERER
 	webglRenderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 	webglRenderer.setSize(WRAP_WIDTH, WRAP_HEIGHT);
@@ -42,26 +29,28 @@ function initModel()
 
 	//LOAD MODEL AND UV
 	var loader = new THREE.JSONLoader();
-	var callbackKey = function (geometry) { createScene(geometry, 0, -5, 0, 3, "model/mechUV.jpg") };
-	loader.load("model/mech.js", callbackKey);
+	var callbackKey = function (geometry) { createScene(geometry, 0, 0, 0, 2, "model/testUV.jpg") };
+	loader.load("model/figure.js", callbackKey);
 }
 
 
 function createScene(geometry, x, y, z, scale, tmap)
 {
-	zmesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({ map: THREE.ImageUtils.loadTexture(tmap) }));
-	zmesh.position.set(x, y, z);
-	zmesh.scale.set(scale, scale, scale);
-	scene.add(zmesh);
+	var materials = [
+		new THREE.MeshPhongMaterial({ map: THREE.ImageUtils.loadTexture(tmap) }),
+		new THREE.MeshPhongMaterial({ opacity: 0.25, transparent: true }),
+		new THREE.MeshPhongMaterial({ map: THREE.ImageUtils.loadTexture(tmap) })
+	];
+	figureMesh = new THREE.Mesh(geometry, new THREE.MultiMaterial(materials));
+	figureMesh.position.set(x, y, z);
+	figureMesh.scale.set(scale, scale, scale);
+	scene.add(figureMesh);
 }
 function render()
 {
-	//PARTICLES
-	//TWEEN.update();
-
 	//CAMERA
-	camera.position.x = (mouseX) * -.025;
-	camera.position.y = (mouseY) * .025;
+	camera.position.x = (mouseX) * -.1;
+	camera.position.y = (mouseY) * .1;
 	camera.lookAt(scene.position);
 	webglRenderer.render(scene, camera);
 }
@@ -70,61 +59,6 @@ function animateModel()
 	requestAnimationFrame(animateModel);
 	render();
 }
-
-
-function generateSprite()
-{
-	var canvas = document.createElement('canvas');
-	canvas.width = 8;
-	canvas.height = 16;
-
-	var context = canvas.getContext('2d');
-	var gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2);
-	gradient.addColorStop(0, 'rgba(255,255,255,1)');
-	gradient.addColorStop(1, 'rgba(255,255,255,0)');
-
-	context.fillStyle = gradient;
-	context.fillRect(0, 0, canvas.width, canvas.height);
-
-	return canvas;
-}
-
-function initParticle(particle, delay)
-{
-
-	var particle = this instanceof THREE.Sprite ? this : particle;
-	var delay = delay !== undefined ? delay : 0;
-
-	particle.position.set(0, -1000, -1500);
-	particle.scale.x = particle.scale.y = Math.random() * 8 + 16;
-
-	new TWEEN.Tween(particle)
-		.delay(delay)
-		.to({}, 10000)
-		.onComplete(initParticle)
-		.start();
-
-	new TWEEN.Tween(particle.position)
-		.delay(delay)
-		.to({ x: Math.random() * 4000 - 2000, y: Math.random() * 4300 , z: Math.random() * 4000 }, 10000)
-		.start();
-
-	new TWEEN.Tween(particle.scale)
-		.delay(delay)
-		.to({ x: 0.01, y: 0.01 }, 10000)
-		.start();
-
-}
-
-
-
-
-
-
-
-
-
-
 
 
 
