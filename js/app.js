33960140507ce48d9ba9247a1fb2def8158ec6f1
richhyaa <richhuynhdev@gmail.com>
2017-09-12 17:10:02 -0700

    Major Update

--- js/app.js
+++ js/app.js
@@ -1,36 +1,19 @@
-
-var WRAP_WIDTH;
-var WRAP_HEIGHT;
-var mouseX = 0;
-var mouseY = 0;
-
-//Events
+﻿
+//Initialize
 $(document).ready(function () {
 
-	//Load Model
-	updateWrapWidthHeight();
-	initModel();
-	animateModel();
-
-	//ADD EVENT LISTENERS
-	initRayPolyHover()
-	window.addEventListener("scroll", onScroll, false);
-	window.addEventListener('resize', onWindowResize, false);
-	window.addEventListener('mousemove', onMouseMove, false);
-
+	preload();
+	initializeGallery();
 });
 
-//Looking for more efficent handler
-function onScroll()
-{
-	headerFade();
-}
-function onMouseMove(event)
-{
-	MouseMoveModel(event);
-};
-function onWindowResize()
-{
-	WindowResizeModel();
+
+function preload() {
+	$("#preload_container").fadeOut(400);
 }
 
+
+function initializeGallery()
+{
+	$('#work_gallery').lightGallery();
+
+}
